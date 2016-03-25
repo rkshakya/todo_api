2f3b5346a383aa42eb7e58cb92e747c2ce5d78e8
Ravi Kishor Shakya <ravi.shakya@gmail.com>
2016-03-25 13:54:13 +0545

    Added PUT

--- server.js
+++ server.js
@@ -63,7 +63,32 @@ app.post('/todos', function(req, res){
 	res.json(body);
 })
 
+app.put('/todos/:id', function(req, res){
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+	var todoId = parseInt(req.params.id, 10);
+	var matchedData = _.findWhere(todos, { id : todoId});
 
+	if(!matchedData){
+		return res.status(404).send();
+	}
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')){
+		return res.status(400).send();
+	}
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0 ){
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')){
+		return res.status(400).send();
+	}
+
+	_.extend(matchedData, validAttributes);
+	res.json(matchedData);
+
+})
 
 app.listen(PORT, function(){
 	console.log('Express listening on port ' + PORT);
