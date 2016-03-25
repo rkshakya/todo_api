e7a6549d4f71666dd85f3039416d2b5c1ea46ca7
Ravi Kishor Shakya <ravi.shakya@gmail.com>
2016-03-25 11:49:58 +0545

    added POST method

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id : 1,
-	description : 'Meet mom',
-	completed : false
-}, {
-	id : 2,
-	description :"Go to market",
-	completed : false
-}, {
-	id : 3,
-	description : "Do yoga",
-	completed : true
-}
-];
+var todos = [];
+var todoId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res){
 	res.send('Todo API root');
@@ -41,6 +33,16 @@ app.get('/todos/:id', function(req, res){
 
 	res.status(404).send();
 
+})
+
+app.post('/todos', function(req, res){
+	var body = req.body;
+	// console.log(body.description);
+	body.id = todoId;
+	todos.push(body);
+	todoId++;
+
+	res.json(body);
 })
 
 
