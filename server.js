c931a7196c2db8dd1e66df325fc06d11fbbd0735
Ravi Kishor Shakya <ravi.shakya@gmail.com>
2016-03-27 12:04:01 +0545

    Added todos to sequelize

--- server.js
+++ server.js
@@ -16,22 +16,28 @@ app.get('/', function (req, res){
 
 //GET /todos
 app.get('/todos', function (req, res){
-	var queryParams = req.query;
-	var filteredTodos = todos;
+	var query = req.query;
+	var where = {};
 
-	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true'){
-		filteredTodos = _.where(todos, { completed : true});
-	}else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false'){
-		filteredTodos = _.where(todos, { completed : false});
+	if (query.hasOwnProperty('completed') && query.completed === 'true'){
+		where.completed = true;
+	} else if (query.hasOwnProperty('completed') && query.completed === 'false'){
+		where.completed = false;
 	}
 
-	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0){
-		filteredTodos = _.filter(filteredTodos, function(q){ 
-			return q.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-		});
+
+	if (query.hasOwnProperty('q') && query.q.length > 0){
+		where.description = {
+			$like : '%' + query.q + '%'
+		};
 	}
 
-	res.json(filteredTodos);
+	db.todo.findAll({where: where}).then (function (todos){
+		res.json(todos);
+	}, function(e){
+		res.status(500).send();
+	});
+
 });
 
 //GET /todos/:id
