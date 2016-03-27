b7bda00edbd1f012ac8a3a58381c9a6a38ecdcd3
Ravi Kishor Shakya <ravi.shakya@gmail.com>
2016-03-27 13:55:20 +0545

    Added PUT to sequelize

--- server.js
+++ server.js
@@ -90,30 +90,32 @@ app.post('/todos', function(req, res){
 
 app.put('/todos/:id', function(req, res){
 	var body = _.pick(req.body, 'description', 'completed');
-	var validAttributes = {};
+	var attributes = {};
 	var todoId = parseInt(req.params.id, 10);
-	var matchedData = _.findWhere(todos, { id : todoId});
 
-	if(!matchedData){
-		return res.status(404).send();
-	}
+	if (body.hasOwnProperty('completed')){
+		attributes.completed = body.completed;
+	} 
 
-	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
-		validAttributes.completed = body.completed;
-	} else if (body.hasOwnProperty('completed')){
-		return res.status(400).send();
-	}
+	if (body.hasOwnProperty('description')){
+		attributes.description = body.description;
+	} 
 
-	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0 ){
-		validAttributes.description = body.description;
-	} else if (body.hasOwnProperty('description')){
-		return res.status(400).send();
-	}
+	db.todo.findById(todoId).then(function(todo){
+		if(todo){
+			todo.update(attributes).then(function(todo){
+				res.json(todo.toJSON());
+			}, function(e){
+				res.status(400).json(e);
+			});
+		} else {
+			res.status(404).send()
+		}
+	}, function(){
+		res.status(500).send()
+	});
 
-	_.extend(matchedData, validAttributes);
-	res.json(matchedData);
-
-})
+});
 
 
 
