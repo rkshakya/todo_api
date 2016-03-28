badbc6cc77bd50cd1c3722c183b0a9ae16fd7d64
Ravi Kishor Shakya <ravi.shakya@gmail.com>
2016-03-28 09:28:54 +0545

    Hash and salt pwd

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ app.post('/users', function(req, res){
 	body = _.pick(body, 'email', 'password');
 	
 	db.user.create(body).then(function(user){
-		res.json(user.toJSON());			
+		res.json(user.toPublicJSON());			
 	}, function(e){
 		res.status(400).json(e);
 	});
